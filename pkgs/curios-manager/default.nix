2339a2fa5d3a13463ae08a20b955554728cd7ce0
VideoCurio <david@videocurio.com>
2025-10-26 14:37:32 +0100

    Added desktop shortcut and icon for curios-manager.

--- pkgs/curios-manager/default.nix
+++ pkgs/curios-manager/default.nix
@@ -10,17 +10,31 @@ stdenvNoCC.mkDerivation rec {
     root = ./.;
     fileset = lib.fileset.unions [
       ./bin
+      ./share
     ];
   };
 
   dontPatch = true;
   dontConfigure = true;
   dontBuild = true;
+  desktopItem = pkgs.makeDesktopItem {
+    name = "com.videocurio.curios.manager";
+    exec = "/run/current-system/sw/bin/alacritty -e curios-manager";
+    desktopName = "CuriOS Manager CLI";
+    icon = "desktop-curios-manager";
+    categories = [ "System" ];
+    terminal = true;
+  };
   installPhase = ''
     runHook preInstall
 
     mkdir -p  $out/bin/
     install -D -m 555 -t $out/bin/ bin/curios-manager
+
+    mkdir -p $out/share
+    cp -r ${desktopItem}/share/applications $out/share
+    mkdir -p $out/share/icons/hicolor/scalable/apps
+    cp share/icons/hicolor/scalable/apps/nixos.svg $out/share/icons/hicolor/scalable/apps/desktop-curios-manager.svg
 
     runHook postInstall
   '';
