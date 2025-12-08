1e31d1d682b5d31c099bad05de13d4f78bc8d41e
VideoCurio <david@videocurio.com>
2025-12-08 16:20:52 +0100

    Renamed some files test.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 nix-build ./backup.nix --show-trace
-nix-build ./basics-all-options.nix --show-trace
+nix-build ./basics.nix --show-trace
 nix-build ./cosmic.nix --show-trace
-nix-build ./crypto-all-options.nix --show-trace
+nix-build ./crypto.nix --show-trace
 nix-build ./curios-pkgs.nix --show-trace
-nix-build ./devops-all-options.nix --show-trace
+nix-build ./devops.nix --show-trace
 nix-build ./fonts.nix --show-trace
-nix-build ./gaming-all-options.nix --show-trace
+nix-build ./gaming.nix --show-trace
 nix-build ./networking.nix --show-trace
-nix-build ./office-all-options.nix --show-trace
+nix-build ./office.nix --show-trace
 nix-build ./services-ai.nix --show-trace
 nix-build ./services.nix --show-trace
-nix-build ./studio-all-options.nix --show-trace
+nix-build ./studio.nix --show-trace
 nix-build ./virtualisation-podman.nix --show-trace
 nix-build ./virtualisation.nix --show-trace
 nix-build ./zsh.nix --show-trace
