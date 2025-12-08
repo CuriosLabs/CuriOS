50f0af7aa1d89a475cbea27227a1f9213e5c044c
VideoCurio <david@videocurio.com>
2025-12-08 15:10:40 +0100

    Added services tests.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -9,6 +9,7 @@ nix-build ./fonts.nix --show-trace
 nix-build ./gaming-all-options.nix --show-trace
 nix-build ./office-all-options.nix --show-trace
 nix-build ./services-ai.nix --show-trace
+nix-build ./services.nix --show-trace
 nix-build ./studio-all-options.nix --show-trace
 #nix repl ./basics-all-options.nix
 #&& nix-instantiate ./basics-all-options.nix
