e24de672a776b920e1b22afb98125c40f5f63f13
VideoCurio <david@videocurio.com>
2025-12-08 11:05:14 +0100

    Added CuriOS pkgs tests.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -3,6 +3,7 @@ nix-build ./backup.nix --show-trace
 nix-build ./basics-all-options.nix --show-trace
 nix-build ./cosmic.nix --show-trace
 nix-build ./crypto-all-options.nix --show-trace
+nix-build ./curios-pkgs.nix --show-trace
 nix-build ./devops-all-options.nix --show-trace
 nix-build ./gaming-all-options.nix --show-trace
 nix-build ./office-all-options.nix --show-trace
