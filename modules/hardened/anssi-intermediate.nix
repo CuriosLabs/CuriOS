52de1b9f7c531120f79b9b60b31fa37cde23da27
VideoCurio <david@videocurio.com>
2026-05-06 09:47:51 +0200

    Fix: Linux kernel lsm options

--- modules/hardened/anssi-intermediate.nix
+++ modules/hardened/anssi-intermediate.nix
@@ -109,12 +109,9 @@
       # Linux Security Modules (LSM) configuration.
       # In NixOS 26.05+, security.lsm defines the initialization order.
       # We ensure 'capability' is first, followed by others.
-      lsm = lib.optionals config.curios.hardened.anssi.intermediate.rule11 [
-        "capability"
-        "landlock"
-        "yama"
-        "bpf"
-      ];
+      # "landlock", "yama", "bpf" are already enable by NixOS 25.11+
+      lsm = lib.optionals config.curios.hardened.anssi.intermediate.rule11
+        [ "capability" ];
 
       sudo = lib.mkIf config.curios.hardened.anssi.intermediate.rule39 {
         extraConfig = ''
