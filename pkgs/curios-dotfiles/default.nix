9791b4ab1b40ebd350994d10c00e042f7cb17299
VideoCurio <david@videocurio.com>
2026-05-12 10:41:34 +0200

    Updated: curios-dotfiles to v0.29.1

--- pkgs/curios-dotfiles/default.nix
+++ pkgs/curios-dotfiles/default.nix
@@ -4,13 +4,13 @@
 { lib, stdenvNoCC, fetchFromGitHub }:
 stdenvNoCC.mkDerivation rec {
   pname = "curios-dotfiles";
-  version = "0.28.2";
+  version = "0.29.1";
 
   src = fetchFromGitHub {
     owner = "CuriosLabs";
     repo = "curios-dotfiles";
     rev = version;
-    hash = "sha256-ERCc0zZi8aTPPkEbCWbvhHJOZ92C8RAMig03WK13a1U=";
+    hash = "sha256-ZkRd95v+UrWvfHl/NuhAKLK9rMSY788mqC7MbTcf0zk=";
   };
 
   dontPatch = true;
