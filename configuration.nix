4eb72c0e69714d4eb68dfb0b83653258b4efa85d
VideoCurio <david@videocurio.com>
2026-06-09 16:28:30 +0200

    Upgraded: curios-manager to 0.41.5

--- configuration.nix
+++ configuration.nix
@@ -143,7 +143,7 @@ in {
     copySystemConfiguration = true;
     # CuriOS variant version
     nixos.variantName = "CuriOS";
-    nixos.variant_id = "unstable-20260606.1220";
+    nixos.variant_id = "unstable-20260609.0754";
   };
 
   nix = {
--- pkgs/curios-sources/default.nix
+++ pkgs/curios-sources/default.nix
@@ -8,7 +8,7 @@
 { lib, stdenvNoCC }:
 stdenvNoCC.mkDerivation {
   pname = "curios-sources";
-  version = "unstable-20260606.1220";
+  version = "unstable-20260609.0754";
 
   src = lib.fileset.toSource {
     root = ../../.;
