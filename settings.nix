a3088b81c60b51aaa2097516d0e211822d75bbb1
VideoCurio <david@videocurio.com>
2026-08-02 12:06:54 +0200

    Added: user to render group if NVidia or AMD GPU is enable.

--- settings.nix
+++ settings.nix
@@ -37,7 +37,9 @@
       extraGroups =
         [ "users" "wheel" "audio" "sound" "video" "plugdev" "dialout" ]
         ++ lib.optionals config.curios.desktop.crypto.enable [ "tty" ]
-        ++ lib.optionals config.curios.hardware.various.i2c.enable ["i2c"]
+        ++ lib.optionals (config.curios.hardware.amdGpu.enable
+          || config.curios.hardware.nvidiaGpu.enable) [ "render" ]
+        ++ lib.optionals config.curios.hardware.various.i2c.enable [ "i2c" ]
         ++ lib.optionals config.curios.networking.enable [ "networkmanager" ]
         ++ lib.optionals config.curios.virtualisation.enable [
           "libvirtd"
