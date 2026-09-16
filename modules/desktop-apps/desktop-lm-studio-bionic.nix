2ce5fe9da5a42b081045ba914b75aaa462749bf3
VideoCurio <david@videocurio.com>
2026-09-16 11:45:42 +0200

    Upgraded: LM Studio and LM Studio Bionic to 1.1.3-5

--- modules/desktop-apps/desktop-lm-studio-bionic.nix
+++ modules/desktop-apps/desktop-lm-studio-bionic.nix
@@ -5,14 +5,14 @@
 { pkgs, lib }:
 let
   pname = "lm-studio-bionic";
-  version = "1.1.2-11";
+  version = "1.1.3-5";
 
   # Calculate the hash with:
   # nix --extra-experimental-features nix-command hash convert --hash-algo sha256 "$(nix-prefetch-url https://bionic-installers.lmstudio.ai/linux/x64/1.1.1-5/Bionic-1.1.1-5-x64.AppImage)"
   src = pkgs.fetchurl {
     url =
       "https://bionic-installers.lmstudio.ai/linux/x64/${version}/Bionic-${version}-x64.AppImage";
-    hash = "sha256-g1NDcroAtP7sU+ea3dq0bZ1oyvQLUxbpl+ONFbDhwME=";
+    hash = "sha256-0rmi8lB2caV4LP64BmxusQpNsmdIH6MMhXX3oo88iZg=";
   };
 
   appimageContents = pkgs.appimageTools.extract { inherit pname version src; };
